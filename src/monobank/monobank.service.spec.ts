780f3baf25ee3fcba30ec20ee11a8e7f0d7bd1ac
mac <denis.davydov.od@gmail.com>
2025-01-13 01:24:02 +0100

    Unit Tests, Test Coverage for Monobank service

--- src/monobank/monobank.service.spec.ts
+++ src/monobank/monobank.service.spec.ts
@@ -1,18 +1,204 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { MonobankService } from './monobank.service';
+import {Test, TestingModule} from '@nestjs/testing';
+import {MonobankService} from './monobank.service';
+import {RedisService} from '@/redis/redis.service';
+import * as process from "node:process";
 
 describe('MonobankService', () => {
-  let service: MonobankService;
+    let service: MonobankService;
+    let redisService: RedisService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [MonobankService],
-    }).compile();
+    beforeEach(async () => {
+        process.env.MONOBANK_API_URL = 'https://api.monobank.ua/bank/currency';
+        process.env.MONOBANK_RATES_TTL = '60';
 
-    service = module.get<MonobankService>(MonobankService);
-  });
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                MonobankService,
+                {
+                    provide: RedisService,
+                    useValue: {
+                        set: jest.fn(),
+                        getObject: jest.fn(),
+                        get: jest.fn(),
+                    },
+                },
+            ],
+        }).compile();
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+        service = module.get<MonobankService>(MonobankService);
+        redisService = module.get<RedisService>(RedisService);
+    });
+
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
+
+    it('updateRates should throw error with errorDescription', async () => {
+        jest.spyOn(global, 'fetch').mockImplementation(() =>
+            Promise.resolve({
+                json: () => Promise.resolve({errorDescription: 'Fetch error'}),
+            } as Response)
+        );
+
+        await expect(service.updateRates()).rejects.toThrow('Fetch error');
+    });
+
+    it('updateRates should throw error on empty result', async () => {
+        jest.spyOn(global, 'fetch').mockImplementation(() =>
+            Promise.resolve({
+                json: () => Promise.resolve([]),
+            } as Response)
+        );
+
+        await expect(service.updateRates()).rejects.toThrow('Empty rates response');
+    });
+
+    it('set redis value on success fetch', async () => {
+        const testRates = [
+            {currencyCodeA: 1, currencyCodeB: 2, rateBuy: 2.5, rateSell: 2.7},
+            {currencyCodeA: 1, currencyCodeB: 3, rateBuy: 3.5, rateSell: 3.7},
+        ];
+
+        jest.spyOn(global, 'fetch').mockImplementation(() =>
+            Promise.resolve({
+                json: () => Promise.resolve(testRates),
+            } as Response)
+        );
+
+        jest.spyOn(redisService, 'set').mockImplementation((key, value, ttl): Promise<void> => {
+                expect(value).toEqual(testRates);
+                expect(ttl).toEqual(Number(process.env.MONOBANK_RATES_TTL));
+                expect(key).toEqual('monobank');
+                return Promise.resolve();
+        });
+
+        await service.updateRates();
+    });
+
+    it('getRates should return cached data if available', async () => {
+        const cachedData = {rates: [
+            {currencyCodeA: 1, currencyCodeB: 2, rateBuy: 2.5, rateSell: 2.7},
+            {currencyCodeA: 1, currencyCodeB: 3, rateBuy: 3.5, rateSell: 3.7},
+        ]};
+
+        jest.spyOn(redisService, 'getObject').mockResolvedValue(cachedData);
+
+        const result = await service.getRates();
+
+        expect(result).toEqual(cachedData);
+
+        expect(redisService.getObject).toHaveBeenCalledWith('monobank');
+    });
+
+    it('getRates should call updateRates if no cached data', async () => {
+        jest.spyOn(redisService, 'getObject').mockResolvedValue(null);
+
+        const updatedData = {rates: [
+            {currencyCodeA: 1, currencyCodeB: 2, rateBuy: 3.5, rateSell: 3.7},
+            {currencyCodeA: 1, currencyCodeB: 3, rateBuy: 4.5, rateSell: 4.7},
+        ]};
+
+        jest.spyOn(service, 'updateRates').mockResolvedValue(updatedData);
+
+        const result = await service.getRates();
+
+        expect(result).toEqual(updatedData);
+
+        expect(service.updateRates).toHaveBeenCalled();
+    });
+
+    it('convert should return correct conversion for single exchange with rateBuy/rateSell values', async () => {
+        const rates = [
+            {currencyCodeA: 1, currencyCodeB: 3, rateCross: 2.4},
+            {currencyCodeA: 2, currencyCodeB: 3, rateBuy: 3, rateSell: 3.7},
+            {currencyCodeA: 1, currencyCodeB: 2, rateBuy: 2, rateSell: 2.5},
+        ];
+
+        jest.spyOn(service, 'getRates').mockResolvedValue(rates);
+
+        const result12 = await service.convert(100, 1, 2);
+
+        expect(result12).toEqual({
+            amount: 200,
+            type: 'single',
+            exchanges: [1, 2],
+        });
+
+        const result21 = await service.convert(100, 2, 1);
+
+        expect(result21).toEqual({
+            amount: 40,
+            type: 'single',
+            exchanges: [2, 1],
+        });
+    });
+
+    it('convert should return correct conversion for single exchange with rateCross value', async () => {
+        const rates = [
+            {currencyCodeA: 1, currencyCodeB: 3, rateCross: 2.4},
+            {currencyCodeA: 1, currencyCodeB: 4, rateBuy: 2, rateSell: 2.7},
+            {currencyCodeA: 1, currencyCodeB: 2, rateCross: 2},
+        ];
+
+        jest.spyOn(service, 'getRates').mockResolvedValue(rates);
+
+        const result12 = await service.convert(100, 1, 2);
+
+        expect(result12).toEqual({
+            amount: 200,
+            type: 'single',
+            exchanges: [1, 2],
+        });
+
+        const result21 = await service.convert(100, 2, 1);
+
+        expect(result21).toEqual({
+            amount: 50,
+            type: 'single',
+            exchanges: [2, 1],
+        });
+    });
+
+    it('convert should return correct conversion for double exchange', async () => {
+        const rates = [
+            {currencyCodeA: 1, currencyCodeB: 2, rateBuy: 3, rateSell: 3.1},
+            {currencyCodeA: 3, currencyCodeB: 2, rateCross: 2},
+        ];
+
+        jest.spyOn(service, 'getRates').mockResolvedValue(rates);
+
+        const result = await service.convert(100, 1, 3);
+
+        expect(result).toEqual({
+            amount: 150,
+            type: 'double',
+            exchanges: [1, 2, 3],
+        });
+    });
+
+    it('reject when rates not found', async () => {
+        const rates = [
+            {currencyCodeA: 1, currencyCodeB: 2, rateBuy: 3, rateSell: 3.1},
+            {currencyCodeA: 3, currencyCodeB: 2, rateCross: 2},
+        ];
+
+        jest.spyOn(service, 'getRates').mockResolvedValue(rates);
+
+        await expect(service.convert(100, 2, 4)).rejects.toThrow('Exchange direction not found');
+    });
+
+    it('contain equal amount of currencies in availableCurrencies', async () => {
+        const rates = [
+            {currencyCodeA: 1, currencyCodeB: 2, rateBuy: 3, rateSell: 3.1},
+            {currencyCodeA: 3, currencyCodeB: 2, rateCross: 2},
+            {currencyCodeA: 3, currencyCodeB: 5, rateBuy: 3, rateSell: 3.1},
+            {currencyCodeA: 7, currencyCodeB: 9, rateCross: 2},
+        ];
+
+        jest.spyOn(service, 'getRates').mockResolvedValue(rates);
+
+        const result = await service.availableCurrencies();
+
+        expect(result.length).toEqual(6);
+    });
 });
--- src/monobank/monobank.service.ts
+++ src/monobank/monobank.service.ts
@@ -1,6 +1,5 @@
-import { Injectable, Logger } from '@nestjs/common';
+import {Injectable, Logger} from '@nestjs/common';
 import {RedisService} from "@/redis/redis.service";
-import {from} from "rxjs";
 import * as process from "node:process";
 
 @Injectable()
@@ -8,13 +7,17 @@ export class MonobankService {
 
     private readonly logger = new Logger(MonobankService.name);
 
-    constructor(private readonly redisService: RedisService) {}
+    constructor(private readonly redisService: RedisService) {
+    }
 
     async updateRates(): Promise<any> {
         let data = await fetch(process.env.MONOBANK_API_URL);
         let json = await data.json();
         if (json.errorDescription) {
             throw new Error(json.errorDescription);
+        }
+        if (!json.length) {
+            throw new Error('Empty rates response');
         }
         await this.redisService.set('monobank', json, Number(process.env.MONOBANK_RATES_TTL));
         this.logger.log('Rates updated');
@@ -33,47 +36,52 @@ export class MonobankService {
     async availableCurrencies(): Promise<any> {
         let rates = await this.getRates();
         let currencies = rates.map((rate: any) => rate.currencyCodeA).concat(rates.map((rate: any) => rate.currencyCodeB));
-        return [...new Set(currencies)].sort((a:number, b:number) => a - b);
+        return [...new Set(currencies)].sort((a: number, b: number) => a - b);
     }
 
-    async convert(amount: number, fromCurrency: number, toCurrency: number): Promise<{ type:string, amount:number, exchanges:Array<number> }> {
+    async convert(amount: number, fromCurrency: number, toCurrency: number): Promise<{
+        type: string,
+        amount: number,
+        exchanges: Array<number>
+    }> {
         let rates = await this.getRates();
-        this.logger.log('Rates qty: ' + (rates && rates.length) );
+        this.logger.log('Rates qty: ' + (rates && rates.length));
 
         const ratesMap = new Map();
         for (let rate of rates) {
             if (rate.rateCross) {
-                // В случае кросс-курса, оба направления валидны, курс одинаковый, можно конечно сделать иначе
-                ratesMap.set(rate.currencyCodeA+'-'+rate.currencyCodeB, rate.rateCross);
-                ratesMap.set(rate.currencyCodeB+'-'+rate.currencyCodeA, rate.rateCross);
+                // В случае кросс-курса, оба направления валидны, курс одинаковый, маржи нет
+                ratesMap.set(rate.currencyCodeA + '-' + rate.currencyCodeB, rate.rateCross);
+                ratesMap.set(rate.currencyCodeB + '-' + rate.currencyCodeA, 1 / rate.rateCross);
             } else {
-                // В случае наличия курса покупки и продажи, используем их
-                ratesMap.set(rate.currencyCodeA+'-'+rate.currencyCodeB, rate.rateBuy);
-                ratesMap.set(rate.currencyCodeB+'-'+rate.currencyCodeA, rate.rateSell);
+                // В случае наличия курса покупки и продажи
+                // rateBuy - курс покупки банком валюты, rateSell - курс продажи банком валюты
+                ratesMap.set(rate.currencyCodeA + '-' + rate.currencyCodeB, rate.rateBuy);
+                ratesMap.set(rate.currencyCodeB + '-' + rate.currencyCodeA, 1 / rate.rateSell);
             }
         }
 
-        if (ratesMap.has(fromCurrency+'-'+toCurrency)) {
+        if (ratesMap.has(fromCurrency + '-' + toCurrency)) {
             return {
-                amount: amount * ratesMap.get(fromCurrency+'-'+toCurrency),
+                amount: amount * ratesMap.get(fromCurrency + '-' + toCurrency),
                 type: 'single',
                 exchanges: [fromCurrency, toCurrency]
             }
         }
 
-        // В целом не обязательная часть, но в Монобанке она есть, называется двойная конверсия
+        // В целом не обязательная часть, но в Монобанке она есть, называется двойная конвертация
         for (let rate1 of ratesMap) {
-            let [ currencyFrom1, currencyTo1 ] = rate1[0].split('-');
+            let [currencyFrom1, currencyTo1] = rate1[0].split('-');
             if (Number(currencyFrom1) === fromCurrency) {
                 for (let rate2 of ratesMap) {
-                    let [ currencyFrom2, currencyTo2 ] = rate2[0].split('-');
+                    let [currencyFrom2, currencyTo2] = rate2[0].split('-');
                     if (currencyFrom2 == currencyTo1 && Number(currencyTo2) == toCurrency) {
-                        const conversion1 = ratesMap.get(currencyFrom1+'-'+currencyTo1);
-                        const conversion2 = ratesMap.get(currencyFrom2+'-'+currencyTo2);
+                        const conversion1 = ratesMap.get(currencyFrom1 + '-' + currencyTo1);
+                        const conversion2 = ratesMap.get(currencyFrom2 + '-' + currencyTo2);
                         return {
                             amount: amount * conversion1 * conversion2,
                             type: 'double',
-                            exchanges: [currencyFrom1, currencyTo1, currencyTo2]
+                            exchanges: [Number(currencyFrom1), Number(currencyTo1), Number(currencyTo2)]
                         }
                     }
                 }
